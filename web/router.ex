6742644a001ff16e50664a6a167b843989472425
Maximilian Kuschewski <maxi.kuschewski@gmail.com>
2017-04-26 22:04:07 +0200

    Fix route order so that pokemon-route is at the pottom

--- web/router.ex
+++ web/router.ex
@@ -2,6 +2,18 @@ defmodule Canboard.Router do
   use Canboard.Web, :router
   use Coherence.Router
 
+  pipeline :api do
+
+    scope "/api", Canboard do
+      resources "/boards", BoardController, only: [:index, :show, :create, :update, :delete]
+    end
+
+    plug :accepts, ["json"]
+    plug :fetch_session
+    plug :put_secure_browser_headers
+    plug Coherence.Authentication.Session, cookie_expire: 10*60*60, login: &Canboard.PageController.index/1
+  end
+
   pipeline :frontend do
 
     scope "/", Canboard do
@@ -14,18 +26,6 @@ defmodule Canboard.Router do
     plug :fetch_flash
     plug :put_secure_browser_headers
     plug Coherence.Authentication.Session, protected: false
-  end
-
-  pipeline :api do
-
-    scope "/api", Canboard do
-      resources "/boards", BoardController, only: [:index, :show, :create, :update, :delete]
-    end
-
-    plug :accepts, ["json"]
-    plug :fetch_session
-    plug :put_secure_browser_headers
-    plug Coherence.Authentication.Session, cookie_expire: 10*60*60, login: &Canboard.PageController.index/1
   end
 
 end
