508e60738345c60da9d5a1a1c8bcb056c168d462
Magnus Görlitz <magnus.goerlitz@makandra.de>
2017-04-26 21:24:24 +0200

    Change default dev pg login, update migrations

--- config/dev.exs
+++ config/dev.exs
@@ -35,8 +35,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :canboard, Canboard.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "canboard",
-  password: "canboard",
+  username: "postgres",
+  password: "postgres",
   database: "canboard",
   hostname: "localhost",
   pool_size: 10
